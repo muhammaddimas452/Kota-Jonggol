44cfb31058221fbeb648e1c0b420887e3b5e3898
muhammaddimas452 <wdimas452@gmail.com>
2022-04-09 08:55:13 +0700

    hai

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,7 @@ const headers = {
   Accept: "application/json",
 };
 const axiosClient = axios.create({
-  baseURL: "https://16d9-180-244-160-94.ngrok.io/api/",
+  baseURL: "https://1147-180-244-160-94.ap.ngrok.io/api/",
   headers,
 });
 

d5895f4b7aeda5fa20ffa3c0c6a39525d29e1970
muhammaddimas452 <wdimas452@gmail.com>
2022-05-29 16:19:04 +0700

    baru

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,7 @@ const headers = {
   Accept: "application/json",
 };
 const axiosClient = axios.create({
-  baseURL: "http://api-projectakhir.herokuapp.com/api/",
+  baseURL: "https://api-projectakhir.herokuapp.com/api/",
   headers,
 });
 
